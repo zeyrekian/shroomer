f649beb0ca81aa794740a855af84ad4655544a7b
galodronio <galodronio@up.edu.ph>
2025-10-19 13:42:37 +0800

    Now with flamethrowers and mushrooms
    Things implemented:
    - Flamethrowers actually work now
    - Implemented mushrooms actually ending the game when all is collected
    - Terminal clears itself after every move
    
    Things left to implement:
    - Alternate controls (ULDR)
    - Leveldata generator from the base level string
    - Better optimization ig

--- Gabs_shroom_raider.py
+++ Gabs_shroom_raider.py
@@ -1,12 +1,14 @@
+import os
+
 LEVEL = '''\
 TTTTTTTTTTT
 T...T+T...T
 T.*.T~T.x.T
 T.TT.~....T
-T.TxTL.*..T
+T.T+TL.*..T
 T.TTT.....T
-T..R...R..T
-T.........T
+T..R...RTTT
+T.......T+T
 TTTTTTTTTTT
 '''
 
@@ -15,14 +17,14 @@ LEVELDATA = {
 # Will eventually make a command to automatically make leveldata with only the level
 	'borders': (9, 11),
 	'laro': (4, 5),
-	'mushrooms': (),
-	'mush_count': (),
+	'mush_collected': 0,
+	'mush_total': 3,
 	'paved': (),
-	'axe': [(4, 3), (2, 8)],
+	'axe': [(2, 8)],
 	'fire': [(4, 7), (2, 2)],
 	'standing_on': '',
 	'holding': '',
-
+	'move_count': 0
 }
 
 lv = LEVEL
@@ -36,6 +38,8 @@ for x in LEVELDATA:
 
 def move(level, leveldata): 
 # Gets your next input, runs a different command according to your input
+	os.system('clear')
+	if leveldata['mush_collected'] == leveldata['mush_total']: endscreen(leveldata)
 	(laro_r, laro_c) = leveldata['laro']
 	levelgrid = level.split('\n')
 	levelgrid = [list(x) for x in levelgrid]
@@ -52,6 +56,8 @@ def move(level, leveldata):
 [P] = PICK UP
 [!] = RESET
 [Q] = EXIT
+
+Collected {leveldata['mush_collected']} out of {leveldata['mush_total']} mushrooms
 
 Currently standing on: {leveldata['standing_on']}
 You currently have: {leveldata['holding']}
@@ -80,6 +86,7 @@ Choose your next move: ''')
 
 def move_check(level, leveldata, tile_to_move): 
 # Checks the tile Laro is about to move into, runs different commands depending on what laro runs into
+	leveldata['move_count'] += 1
 	(r, c) = leveldata['laro']
 	neighbors = {
 		'w': (r-1, c),
@@ -88,12 +95,19 @@ def move_check(level, leveldata, tile_to_move):
 		'd': (r, c+1),
 	}
 	(r1, c1) = neighbors[tile_to_move] # Tile Laro is about to move to, used to check what kind of tile laro is about to run to
-	if level[r1][c1] == 'T': 
+	if not out_of_borders(r1, c1, leveldata['borders']):return (level, leveldata)
+	elif level[r1][c1] == 'T': 
 		if leveldata['holding']: return use_item(level, leveldata, (r1, c1)) # Runs in case Laro has an item
 		else: return (level, leveldata) # Returns same level state, laro doesnt move (if Laro has no item)
 	elif level[r1][c1] == 'R': return move_rock(level, leveldata, tile_to_move) # Runs rock pushing command
+	elif level[r1][c1] == '+':
+		leveldata['mush_collected'] += 1
+		level[r][c] = '.'
+		level[r1][c1] = 'L'
+		leveldata['laro'] = (r1, c1)
+		return (level, leveldata)
 	elif level[r1][c1] == '~': # Laro dies
-		deathscreen()
+		endscreen(leveldata)
 	else: # If tile is empty, laro moves as usual 
 		level[r][c] = '.'
 		level[r1][c1] = 'L'
@@ -110,7 +124,7 @@ def move_rock(level, leveldata, tile_to_move):
 		'd': (r, c+1, r, c+2),
 	}
 	(r1, c1, r2, c2) = neighbors[tile_to_move] # 1 is the rock, 2 is the tile in front of the rock
-	if level[r2][c2] in ('T', 'R', 'x', '*'): return (level, leveldata) # Doesn't push if the tile the rock is going to is an object
+	if level[r2][c2] in ('T', 'R', 'x', '*', '+'): return (level, leveldata) # Doesn't push if the tile the rock is going to is an object
 	elif level[r2][c2] == '~':
 		level[r][c] = '.'
 		level[r1][c1] = 'L'
@@ -139,22 +153,19 @@ def use_item(level, leveldata, next_tile):
 	(r1, c1) = next_tile
 	if leveldata['holding'] == 'Axe': level[r1][c1] = '.'
 	elif leveldata['holding'] == 'Flamethrower': 
-		flame = flamethrower(level, leveldata, r1, c1, frozenset())
-		print(flame)
-		for rx, cx in flame:
+		for rx, cx in flamethrower(level, leveldata, r1, c1, frozenset()):
 			level[rx][cx] = '.'
 	leveldata['holding'] = ''
 	return (level, leveldata)
 
 def flamethrower(level, leveldata, r, c, trees):
-# NOT DONE YET, WILL FIX SOON
-	trees = frozenset((*trees, (r, c)))
+# FIXED, returns a frozenset of coords of all trees adjacent to (r, c)
 	neighbors = ((r+1, c), (r-1, c), (r, c+1), (r, c-1))
 	level[r][c] = '.'
 	valid_neighbors = tuple((rx, cx) for rx, cx in neighbors if out_of_borders(rx, cx, leveldata['borders']))
 	tree_neighbors = tuple((rx, cx) for rx, cx in valid_neighbors if level[rx][cx] == 'T')
 	if not tree_neighbors: return trees
-	else: return trees.union(flamethrower(level, leveldata, rx, cx, trees) for rx, cx in tree_neighbors)
+	else: return frozenset().union(x for rx, cx in tree_neighbors for x in flamethrower(level, leveldata, rx, cx, frozenset((*trees, (r, c)))))
 		
 def out_of_borders(r, c, borders):
 	(rb, cb) = borders
@@ -162,11 +173,21 @@ def out_of_borders(r, c, borders):
 	else: return True
 
 
-def deathscreen(): 
-# Runs when Laro runs into water and drowns lmao
-	letter = input('''\
+def endscreen(leveldata): 
+# Runs when you encounter an end state (laro dies or you collect all mushies)
+	if leveldata['mush_collected'] == leveldata['mush_total']:
+		letter = input(f'''\
+YOU WON!
 
+Completed in {leveldata['move_count']} moves!
 
+Collected {leveldata['mush_collected']} out of {leveldata['mush_total']} mushrooms
+
+PRESS [!] TO RESET
+PRESS [Q] TO QUIT
+
+''')
+	else: letter = input('''\
 YOU DIED...
 
 PRESS [!] TO RESET
